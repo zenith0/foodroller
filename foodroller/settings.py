402c8901cc5966f6f28187cd4cb52bd1a4308117
stefanperndl <stefan.perndl@bud-and-terence.com>
2015-05-08 11:06:55 +0200

    added responsive design

--- foodroller/settings.py
+++ foodroller/settings.py
@@ -86,3 +86,9 @@ STATIC_URL = '/static/'
 TEMPLATE_DIRS = (
     os.path.join(BASE_DIR,  'templates'),
 )
+
+#STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
+
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
