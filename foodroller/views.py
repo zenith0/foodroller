0ddfd0258ddb956c071ad2f42541a60db9815698
zenith0 <pernstef@gmail.com>
2015-12-09 20:31:20 +0100

    added new templates and created new project structure

---
+++ foodroller/views.py
@@ -0,0 +1,14 @@
+from django.http import HttpResponse
+from django.shortcuts import render, render_to_response
+
+
+def index(request):
+    return render_to_response('index.html')
+
+
+def categories(request):
+    return render_to_response('categories.html')
+
+
+def roll(request):
+    return render_to_response('roll.html')
