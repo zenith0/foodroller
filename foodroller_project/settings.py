c7877f72d6ccbf4d0c24c68a52450171d1477d5e
zenith0 <pernstef@gmail.com>
2016-03-09 12:41:08 +0100

    Update settings.py

--- foodroller_project/settings.py
+++ foodroller_project/settings.py
@@ -110,7 +110,7 @@ STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'static'),
 )
 
-STATIC_ROOT = 'static/'
+STATIC_ROOT = 'static-files/'
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
